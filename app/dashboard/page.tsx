126a65a6f909548067d28f6f3397a128d7e8a9b5
mznrasil <mzn.rasil@gmail.com>
2024-09-06 17:12:25 +0545

    feat: kinde authentication setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
-export default function DashboardIndexPage() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function DashboardIndexPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <div>
-      <h1>Hello from the dashboard page.</h1>
+      <h1>Welcome, {user.given_name} to the dashboard page.</h1>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
-    <div>
+    <div className="p-10">
       <h1>Hello world</h1>
-      <Button>Hello from the button </Button>
+      {user ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <RegisterLink>
+            <Button>Sign up</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Sign in</Button>
+          </LoginLink>
+        </div>
+      )}
     </div>
   );
 }
