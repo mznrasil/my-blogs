b5b0a2be981f717483809feb55b014beac91dc09
Rasil Maharjan <mzn.rasil@gmail.com>
2024-10-20 20:28:09 +0545

    fix: added callback url in create-user

--- app/api/auth/create-user/route.ts
+++ app/api/auth/create-user/route.ts
@@ -22,5 +22,9 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/dashboard");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/dashboard"
+      : "https://my-blogs-rosy-mu.vercel.app/dashboard",
+  );
 }
